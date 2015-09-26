e24c0ce1b98083a5bb034ab4a3bafbf0114b0a49
Philip Rideout <philip@napofearth.com>
2015-09-26 08:48:28 -0700

    Progress with GLSL parsing.

--- src/shader.c
+++ src/shader.c
@@ -3,14 +3,24 @@
 #include <stdio.h>
 #include <string.h>
 #include "verify.h"
+#include "kvec.h"
 
 void par_shader_load_from_buffer(par_buffer* buf)
 {
     const sds PROGRAM = sdsnew("@program ");
 
-    char* contents = par_buffer_lock(buf, PAR_READ);
+    kvec_t(sds) program_args;
+    kv_init(program_args);
+
+    kvec_t(sds) chunk_bodies;
+    kv_init(chunk_bodies);
+
+    kvec_t(sds) chunk_names;
+    kv_init(chunk_names);
+
     int len = par_buffer_length(buf);
     int nlines;
+    char* contents = par_buffer_lock(buf, PAR_READ);
     sds* lines = sdssplitlen(contents, len, "\n", 1, &nlines);
     par_buffer_unlock(buf);
     sds chunk = 0;
@@ -28,20 +38,28 @@ void par_shader_load_from_buffer(par_buffer* buf)
         printf("line-- %s\n", line);
         char* found = strstr(line, PROGRAM);
         if (found) {
-            sds argstring = sdsnew(found + sdslen(PROGRAM));
-            int nargs = sdslen(argstring);
-            sds* args = sdssplitlen(argstring, nargs, ",", 1, &nargs);
-            for (int a = 0; a < nargs; a++) {
-                sdstrim(args[a], " \t");
-            }
-            par_verify(nargs == 3, "@program should have 3 args", 0);
-            sdsfree(argstring);
-            sdsfreesplitres(args, nargs);
+            kv_push(sds, program_args, sdsnew(found + sdslen(PROGRAM)));
         }
     }
 
     sdsfreesplitres(lines, nlines);
     sdsfree(chunk);
+
+    for (int p = 0; p < kv_size(program_args); p++) {
+        sds argstring = kv_A(program_args, p);
+        int nargs = sdslen(argstring);
+        sds* args = sdssplitlen(argstring, nargs, ",", 1, &nargs);
+        for (int a = 0; a < nargs; a++) {
+            sdstrim(args[a], " \t");
+        }
+        par_verify(nargs == 3, "@program should have 3 args", 0);
+        sdsfreesplitres(args, nargs);
+        sdsfree(argstring);
+    }
+
+    kv_destroy(program_args);
+    kv_destroy(chunk_bodies);
+    kv_destroy(chunk_names);
     printf("TODO: parse shader\n");
 }
 
