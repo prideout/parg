3248d1dfaef7c3a4c2c40368f5a69cfb059b6f71
Philip Rideout <philip@napofearth.com>
2015-11-02 19:16:23 -0800

    Remove dependency on kopen.

--- src/asset.c
+++ src/asset.c
@@ -109,9 +109,6 @@ int par_asset_download(const char* filename, sds targetpath) { return 0; }
 #include <fcntl.h>
 #include <unistd.h>
 
-void* kopen(const char* fn, int* _fd);
-int kclose(void* a);
-
 sds par_asset_whereami()
 {
     if (!_exedir) {
@@ -131,34 +128,7 @@ int par_asset_download(const char* filename, sds targetpath)
     sds baseurl = par_asset_baseurl();
     sds fullurl = sdscat(sdsdup(baseurl), filename);
     printf("Downloading %s...\n", fullurl);
-    int source = 0;
-    void* kdfile = kopen(fullurl, &source);
-    if (!source) {
-        return 0;
-    }
-    int dest = open(targetpath, O_WRONLY | O_CREAT, 0644);
-    char buf[BUFSIZ];
-    size_t size;
-    size_t total = 0;
-    size_t elapsed = 0;
-    while ((size = read(source, buf, BUFSIZ)) > 0) {
-        if (size > BUFSIZ) {
-            close(dest);
-            kclose(kdfile);
-            remove(targetpath);
-            return 0;
-        }
-        write(dest, buf, size);
-        total += size;
-        elapsed += size;
-        if (elapsed > 1024 * 1024) {
-            printf("\t%zu bytes so far...\n", total);
-            elapsed = 0;
-        }
-    }
-    printf("\t%zu bytes total.\n", total);
-    close(dest);
-    kclose(kdfile);
+    return par_easycurl_to_file(fullurl, targetpath);
     return 0;
 }
 
